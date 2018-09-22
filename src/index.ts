a298cad653398c60f8ec31128f9a767b9d3e747d
James Quigley <jamesq@axosoft.com>
2018-09-22 11:15:49 -0700

    Initial guessing work done on plane

---
+++ src/index.ts
@@ -0,0 +1,81 @@
+type User = {
+  id: string,
+  email: string,
+  name: string,
+  role: string
+};
+
+type Label = {
+  id: string,
+  text: string,
+  color: {
+    r: number,
+    g: number,
+    b: number
+  }
+};
+
+type Comment = {
+  id: string,
+  user: [User],
+  text: string,
+  date: string
+};
+
+type Activity = {
+  id: string,
+  text: string,
+  date: string
+};
+
+type Column = {
+  id: string,
+  title: string,
+  order: number,
+  cards: [Card]
+};
+
+type Card = {
+  id: string,
+  title: string,
+  description: string,
+  dueDate: string,
+  assignees: [User],
+  labels: [Label],
+  comments: [Comment],
+  activities: [Activity]
+};
+
+type Board = {
+  id: string,
+  users: [User],
+  columns: [Column],
+  labels: [Label]
+};
+
+const Boards = {
+  create: async (name: string) => {
+    // Create a board
+  },
+  delete: async (id: string) => {
+    // Delete a board
+  },
+  list: async (page: number, count: number) => {
+    // List a users boards. Maybe paged?
+  },
+  details: async (id: string) => {
+    // Get a boards details
+  }
+};
+
+
+class Glo {
+  Boards;
+  Cards;
+  Labels;
+
+  constructor(token: string) {
+  }
+}
+
+export default {}
